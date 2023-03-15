c68739695ca961134236676ac8c653707e86825e
LongVu1611 <118415481+LongVu1611@users.noreply.github.com>
2023-03-15 08:47:15 +0700

    Add files via upload

---
+++ QL.sql
@@ -0,0 +1,4 @@
+﻿--Câu 1c :-Sau cùng, nhân viên QL xem lại kết quả NV1 và NV2 đã làm\
+
+select * from Person.PersonPhone where BusinessEntityID = 095 
+select * from Person.PersonPhone where BusinessEntityID = 119
